68f72cec8213adae6b9af1ed8cca25239ba840cf
jonyrock <ilms@live.ru>
2013-12-30 02:10:47 +0400

    continue sumbing

--- client/build/Debug/GNU-Linux-x86/main.o.d
+++ client/build/Debug/GNU-Linux-x86/main.o.d
@@ -1,1 +1,6 @@
-build/Debug/GNU-Linux-x86/main.o: main.cpp
+build/Debug/GNU-Linux-x86/main.o: main.cpp icmp_header.hpp \
+ ipv4_header.hpp
+
+icmp_header.hpp:
+
+ipv4_header.hpp:
--- client/main.cpp
+++ client/main.cpp
@@ -1,42 +1,138 @@
-
 #include <boost/asio.hpp>
-#include <boost/array.hpp>
-//#include <boost/thread.hpp> 
+#include <boost/bind.hpp>
+#include <istream>
 #include <iostream>
-#include <string>
+#include <ostream>
 
-using namespace boost;
+#include "icmp_header.hpp"
+#include "ipv4_header.hpp"
+
+using boost::asio::ip::icmp;
+using boost::asio::deadline_timer;
+
 using namespace std;
 
-asio::io_service io_service;
-asio::ip::tcp::resolver resolver(io_service);
-asio::ip::tcp::socket sock(io_service);
-array<char, 4096> buffer;
+namespace posix_time = boost::posix_time;
 
-void read_handler(const system::error_code& ec, size_t bytes_transferred) {
-    if (!ec) {
-        cout << string(buffer.data(), bytes_transferred) << endl;
-        sock.async_read_some(asio::buffer(buffer), read_handler);
+class DateRequester {
+public:
+
+    DateRequester(boost::asio::io_service& io_service, const char* destination) :
+    resolver_(io_service), socket_(io_service, icmp::v4()),
+    timer_(io_service), sequence_number_(0) {
+
+        icmp::resolver::query query(icmp::v4(), destination, "");
+        destination_ = *resolver_.resolve(query);
+
+        start_send();
+        start_receive();
+
     }
-}
 
-void connect_handler(const system::error_code& ec) {
-    if (!ec) {
-        asio::write(sock,
-                asio::buffer("GET / HTTP 1.1\r\r\nHost: www.highscore.de\r\n\r\n"));
-        sock.async_read_some(asio::buffer(buffer), read_handler);
+private:
+
+    void start_send() {
+        
+        
+        
+        icmp_header timestamp_request;
+        timestamp_request.type(icmp_header::timestamp_request);
+        timestamp_request.code(0);
+        timestamp_request.identifier(get_identifier());
+        timestamp_request.sequence_number(0);
+        
+        vector<boost::uint32_t> body;
+        body.push_back((uint32_t)12);           // Originate Timestamp
+        body.push_back((uint32_t)0);            // Receive Timestamp
+        body.push_back((uint32_t)0);            // Transmit Timestamp
+        
+        compute_checksum(timestamp_request, body.begin(), body.end());
+
+        // Encode the request packet.
+        boost::asio::streambuf request_buffer;
+        std::ostream os(&request_buffer);
+        os << timestamp_request << body;
+
+        // Send the request.
+        time_sent_ = posix_time::microsec_clock::universal_time();
+        socket_.send_to(request_buffer.data(), destination_);
+
+        // Wait up to five seconds for a reply.
+        timer_.expires_at(time_sent_ + posix_time::seconds(3));
+        timer_.async_wait(boost::bind(&DateRequester::handle_timeout, this));
+        
     }
-}
 
-void resolve_handler(const system::error_code & ec, 
-        asio::ip::tcp::resolver::iterator it) {
-    if(!ec) {
-        sock.async_connect(*it, connect_handler);
+    void handle_timeout() {
+        // Requests must be sent no less than one second apart.
+        timer_.expires_at(time_sent_ + posix_time::seconds(1));
+        timer_.async_wait(boost::bind(&DateRequester::start_send, this));
     }
-}
 
-int main() {
-    asio::ip::tcp::resolver::query query("www.highscore.de", "80");
-    resolver.async_resolve(query, resolve_handler);
-    io_service.run();
+    void start_receive() {
+        // Discard any data already in the buffer.
+        reply_buffer_.consume(reply_buffer_.size());
+
+        // Wait for a reply. We prepare the buffer to receive up to 64KB.
+        socket_.async_receive(reply_buffer_.prepare(65536),
+                boost::bind(&DateRequester::handle_receive, this, _2));
+    }
+
+    void handle_receive(std::size_t length) {
+        // The actual number of bytes received is committed to the buffer so that we
+        // can extract it using a std::istream object.
+        reply_buffer_.commit(length);
+
+        // Decode the reply packet.
+        std::istream is(&reply_buffer_);
+        ipv4_header ipv4_hdr;
+        icmp_header icmp_hdr;
+        is >> ipv4_hdr >> icmp_hdr;
+
+        // We can receive all ICMP packets received by the host, so we need to
+        // filter out only the echo replies that match the our identifier and
+        // expected sequence number.
+        if (is && icmp_hdr.type() == icmp_header::timestamp_reply
+                && icmp_hdr.identifier() == get_identifier()
+                && icmp_hdr.sequence_number() == sequence_number_) {
+            // If this is the first reply, interrupt the five second timeout.
+
+            // Print out some information about the reply packet.
+            posix_time::ptime now = posix_time::microsec_clock::universal_time();
+            
+            cout << "got something" << endl;
+
+        }
+
+        start_receive();
+    }
+
+    static inline unsigned short get_identifier() {
+        return static_cast<unsigned short> (::getpid());
+    }
+
+    icmp::resolver resolver_;
+    icmp::endpoint destination_;
+    icmp::socket socket_;
+    deadline_timer timer_;
+    unsigned short sequence_number_;
+    posix_time::ptime time_sent_;
+    boost::asio::streambuf reply_buffer_;
+};
+
+int main(int argc, char* argv[]) {
+    try {
+        if (argc != 2) {
+            std::cerr << "Usage: ping <host>" << std::endl;
+            return 1;
+        }
+
+        cout << getpid() << endl;
+
+        boost::asio::io_service io_service;
+        DateRequester p(io_service, argv[1]);
+        io_service.run();
+    } catch (std::exception& e) {
+        std::cerr << "Exception: " << e.what() << std::endl;
+    }
 }
